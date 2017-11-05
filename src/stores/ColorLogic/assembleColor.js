9ec0be411d8ed44d7a818c6a29c3d0f502999f77
plexey <tomtrinca@protonmail.com>
2017-11-06 00:40:13 +1100

    Refactor to accept and use modifier argument as part of palette modifier feature

--- src/stores/ColorLogic/assembleColor.js
+++ src/stores/ColorLogic/assembleColor.js
@@ -1,16 +1,33 @@
 const randomSaturation = require("./randomSaturation");
 const randomLightness = require("./randomLightness");
 
-function assembleColor(colors) {
+function assembleColor(colors, modifier) {
   const output = [];
-  for (let i = 0; i < colors.length; i++) {
-    output.push({
-      hue: colors[i],
-      saturation: randomSaturation(),
-      lightness: randomLightness()
-    });
+  if (modifier) {
+    const SAT_MAX = modifier.saturationMax;
+    const SAT_MIN = modifier.saturationMin;
+    const LIT_MAX = modifier.lightnessMax;
+    const LIT_MIN = modifier.lightnessMin;
+    const output = [];
+    for (let i = 0; i < colors.length; i++) {
+      output.push({
+        hue: colors[i],
+        saturation: Math.floor(Math.random() * (SAT_MAX - SAT_MIN) + SAT_MIN),
+        lightness: Math.floor(Math.random() * (LIT_MAX - LIT_MIN) + LIT_MIN)
+      });
+    }
+    return output
   }
-  return output;
+  else {
+    for (let i = 0; i < colors.length; i++) {
+      output.push({
+        hue: colors[i],
+        saturation: randomSaturation(),
+        lightness: randomLightness()
+      });
+    }
+    return output;
+  }
 }
 
 module.exports = assembleColor;

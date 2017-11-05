eb5aa89e29a70bcdf55c85650a954a5e2bd028be
plexey <tomtrinca@protonmail.com>
2017-11-06 00:41:48 +1100

    Add hexToHsl module

---
+++ src/stores/ColorLogic/hexToHsl.js
@@ -0,0 +1,46 @@
+function hexToHsl(hex) {
+  const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  if (result === null) {
+    return;
+  } else {
+    let r;
+    let g;
+    let b;
+    r = parseInt(result[1], 16);
+    g = parseInt(result[2], 16);
+    b = parseInt(result[3], 16);
+    r /= 255;
+    g /= 255;
+    b /= 255;
+    const max = Math.max(r, g, b);
+    const min = Math.min(r, g, b);
+    let h = (max + min) / 2;
+    let s = (max + min) / 2;
+    const l = (max + min) / 2;
+    if (max == min) {
+      h = s = 0; // achromatic
+    } else {
+      const d = max - min;
+      s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+      switch (max) {
+        case r:
+          h = (g - b) / d + (g < b ? 6 : 0);
+          break;
+        case g:
+          h = (b - r) / d + 2;
+          break;
+        case b:
+          h = (r - g) / d + 4;
+          break;
+      }
+      h /= 6;
+    }
+    const HSL = new Object();
+    HSL["h"] = h;
+    HSL["s"] = s;
+    HSL["l"] = l;
+    return HSL;
+  }
+}
+
+module.exports = hexToHsl;

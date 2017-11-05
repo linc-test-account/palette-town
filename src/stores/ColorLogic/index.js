4595b7523731c9e3b4a0e7b318a4c876dcbb54b4
plexey <tomtrinca@protonmail.com>
2017-11-06 00:41:19 +1100

    Add hexToHsl module to index Add hslToRgb module to index Add random module to index

--- src/stores/ColorLogic/index.js
+++ src/stores/ColorLogic/index.js
@@ -1,9 +1,12 @@
 export { default as analogous } from "./analogous";
 export { default as assembleColor } from "./assembleColor";
 export { default as getContrastYIQ } from "./getContrastYIQ";
+export { default as hexToHsl } from "./hexToHsl";
+export { default as hslToRgb } from "./hslToRgb";
 export { default as hslToHex } from "./hslToHex";
 export { default as oneOff } from "./oneOff";
 export { default as pentagon } from "./pentagon";
+export { default as random } from "./random";
 export { default as randomHue } from "./randomHue";
 export { default as randomLightness } from "./randomLightness";
 export { default as randomSaturation } from "./randomSaturation";

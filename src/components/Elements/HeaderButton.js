fba5c101c1507197316780017650ae4c3739cd95
Thomas Trinca <tomtrinca@protonmail.com>
2017-11-10 19:13:29 +1100

    Modofy overlayValue proptype to bool from string
    Formatted with prettier

--- src/components/Elements/HeaderButton.js
+++ src/components/Elements/HeaderButton.js
@@ -1,24 +1,30 @@
-import React, { Component } from 'react';
-import { observer } from 'mobx-react';
-import FontAwesome from 'react-fontawesome';
-import './HeaderButton.css';
-import PropTypes from 'prop-types';
+import React, { Component } from "react";
+import { observer } from "mobx-react";
+import FontAwesome from "react-fontawesome";
+import "./HeaderButton.css";
+import PropTypes from "prop-types";
 
 @observer
 class HeaderButton extends Component {
   static propTypes = {
     btnFunction: PropTypes.func,
     fontAwesomeIcon: PropTypes.string,
-    overlayValue: PropTypes.string
-  }
+    overlayValue: PropTypes.number,
+  };
   render() {
     const { btnFunction, fontAwesomeIcon, overlayValue } = this.props;
     return (
-      <button
-        className="header-button"
-        onClick={btnFunction}>
-        <FontAwesome className="header-icon" name={fontAwesomeIcon} size="2x" />
-        {overlayValue > 0 ? <p className="indicator-overlay">{overlayValue}</p> : ''}
+      <button className="header-button" onClick={btnFunction}>
+        <FontAwesome
+          className="header-icon"
+          name={fontAwesomeIcon}
+          size="2x"
+        />
+        {overlayValue > 0 ? (
+          <p className="indicator-overlay">{overlayValue}</p>
+        ) : (
+          ""
+        )}
       </button>
     );
   }

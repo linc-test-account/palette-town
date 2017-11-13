ef4b44dcae1ac630f2d60d9d4a1dc24bb732c7c4
Thomas Trinca <tomtrinca@protonmail.com>
2017-11-13 12:57:18 +1100

    Replace hslToRgb function with alternative

--- src/stores/ColorLogic/hslToRgb.js
+++ src/stores/ColorLogic/hslToRgb.js
@@ -1,57 +1,50 @@
-// expected hue range: [0, 360)
-// expected saturation range: [0, 1]
-// expected lightness range: [0, 1]
-const hslToRgb = function(hue, saturation, lightness) {
-  // based on algorithm from http://en.wikipedia.org/wiki/HSL_and_HSV#Converting_to_RGB
-  if (hue == undefined) {
-    return [0, 0, 0];
+/**
+     * Converts an HSL color value to RGB. Conversion formula
+     * adapted from http://en.wikipedia.org/wiki/HSL_color_space.
+     * Assumes h, s, and l are contained in the set [0, 1] and
+     * returns r, g, and b in the set [0, 255].
+     *
+     * @param   Number  h       The hue
+     * @param   Number  s       The saturation
+     * @param   Number  l       The lightness
+     * @return  Array           The RGB representation
+     */
+
+function hue2rgb(p, q, t) {
+  if (t < 0) {
+    t += 1;
+  }
+  if (t > 1) {
+    t -= 1;
+  }
+  if (t < 1 / 6) {
+    return p + (q - p) * 6 * t;
+  }
+  if (t < 1 / 2) {
+    return q;
+  }
+  if (t < 2 / 3) {
+    return p + (q - p) * (2 / 3 - t) * 6;
+  }
+  return p;
+}
+
+function hslToRgb(h, s, l) {
+  let r;
+  let g;
+  let b;
+
+  if (s == 0) {
+    r = g = b = l; // achromatic
+  } else {
+    const q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+    const p = 2 * l - q;
+    r = hue2rgb(p, q, h + 1 / 3);
+    g = hue2rgb(p, q, h);
+    b = hue2rgb(p, q, h - 1 / 3);
   }
 
-  const chroma = (1 - Math.abs(2 * lightness - 1)) * saturation;
-  let huePrime = hue / 60;
-  const secondComponent = chroma * (1 - Math.abs(huePrime % 2 - 1));
-
-  huePrime = Math.floor(huePrime);
-  let red;
-  let green;
-  let blue;
-
-  if (huePrime === 0) {
-    red = chroma;
-    green = secondComponent;
-    blue = 0;
-  } else if (huePrime === 1) {
-    red = secondComponent;
-    green = chroma;
-    blue = 0;
-  } else if (huePrime === 2) {
-    red = 0;
-    green = chroma;
-    blue = secondComponent;
-  } else if (huePrime === 3) {
-    red = 0;
-    green = secondComponent;
-    blue = chroma;
-  } else if (huePrime === 4) {
-    red = secondComponent;
-    green = 0;
-    blue = chroma;
-  } else if (huePrime === 5) {
-    red = chroma;
-    green = 0;
-    blue = secondComponent;
-  }
-
-  const lightnessAdjustment = lightness - chroma / 2;
-  red += lightnessAdjustment;
-  green += lightnessAdjustment;
-  blue += lightnessAdjustment;
-
-  return [
-    Math.round(red * 255),
-    Math.round(green * 255),
-    Math.round(blue * 255)
-  ];
-};
+  return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
+}
 
 module.exports = hslToRgb;

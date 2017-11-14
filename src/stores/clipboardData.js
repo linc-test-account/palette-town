2ffb57bfe60b69346c66c89fa18be70a0f2bd98e
Thomas Trinca <tomtrinca@protonmail.com>
2017-11-14 12:12:28 +1100

    Add newline character to hsl.map() to better format contents of hslStrings variable

--- src/stores/clipboardData.js
+++ src/stores/clipboardData.js
@@ -1,13 +1,9 @@
 export default hsl => {
   const hslStrings = hsl.map(
     ({ hue, saturation, lightness }) =>
-      `hsl(${hue}, ${saturation}%, ${lightness})`
+      `hsl(${hue}, ${saturation}%, ${lightness}) \n` 
   );
 
-  // const rgbStrings = rgb.map(({red, green, blue}, index) => (
-  //   `rgb(${red}, ${green}, ${blue})`
-  // ))
-
-  return hslStrings;
+  return hslStrings.join("");
 };
 

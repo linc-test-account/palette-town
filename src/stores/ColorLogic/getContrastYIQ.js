893f5ff38daa20e638076365af79689a9ce12754
Thomas Trinca <tomtrinca@protonmail.com>
2017-11-14 12:19:43 +1100

    Refactor to return 0 or 100, representing lightness in the hsl color space instead of returning strings containing hsla values.

--- src/stores/ColorLogic/getContrastYIQ.js
+++ src/stores/ColorLogic/getContrastYIQ.js
@@ -1,16 +1,12 @@
-function getContrastYIQ(hexcolor, opacity, reverse) {
+function getContrastYIQ(hexcolor) {
   try {
     const myString = hexcolor.substring(1);
     const r = parseInt(myString.substr(0, 2), 16);
     const g = parseInt(myString.substr(2, 2), 16);
     const b = parseInt(myString.substr(4, 2), 16);
     const yiq = (r * 299 + g * 587 + b * 114) / 1000;
-    if (reverse === true) {
-      return yiq >= 128 ? `hsla(0, 0%, 100%, ${opacity})` : `hsla(0, 0%, 0%,  ${opacity})`;
-    }
-    if (reverse === false) {
-      return yiq >= 128 ? `hsla(0, 0%, 0%, ${opacity})` : `hsla(0, 0%, 100%,  ${opacity})`;
-    }
+
+    return yiq >= 128 ? 0 : 100;
   } catch (e) {
     console.log(e);
   }

de57ff57ace61d2e693115e3f40a8102681a3129
Thomas Trinca <tomtrinca@protonmail.com>
2017-11-15 19:00:14 +1100

    Add Color.js - complete reimplimentation of the original Color class located in Data.js. New class allows for switching between color spaces such that the user can choose to manipulate individual hsl values or cmyk values as they please without constant intermediate color space conversions. Can also be extended easily with new color spaces added as new objects to colorSpaces array.

---
+++ src/stores/Color.js
@@ -0,0 +1,79 @@
+import { observable, computed, extendObservable } from "mobx";
+import { getContrastYIQ } from "./ColorLogic";
+import pascalCase from "pascal-case";
+import convert from "color-convert";
+import namer from "color-namer";
+
+const colorSpaces = {
+  rgb: ["red", "green", "blue"],
+  hsl: ["hue", "saturation", "lightness"],
+  cmyk: ["cyan", "magenta", "yellow", "key"],
+  hex: ["hex"]
+};
+
+export default class Color {
+  @observable properties = [];
+  @observable colorSpace = "rgb";
+
+  constructor(initialColorSpace, ...properties) {
+    this.properties = properties;
+    this.colorSpace = initialColorSpace;
+
+    if (!Object.keys(colorSpaces).includes(initialColorSpace)) {
+      throw new Error(`Invalid colorSpace of ${initialColorSpace} used`);
+    }
+    if (properties.length === 0) {
+      throw new Error(`Missing initial color`);
+    }
+
+    Object.keys(colorSpaces).forEach(colorSpace => {
+      colorSpaces[colorSpace].forEach((name, index) => {
+        extendObservable(this, {
+          [name]: computed(() => {
+            if (this.colorSpace === colorSpace) {
+              return this.properties[index];
+            }
+            const result = convert[this.colorSpace][colorSpace](
+              ...this.properties
+            );
+            if (Array.isArray(result)) {
+              return result[index];
+            }
+            return result;
+          })
+        });
+        this[`set${pascalCase(name)}`] = value => {
+          if (this.colorSpace === colorSpace) {
+            this.properties[index] = value;
+            return;
+          }
+          const result = convert[this.colorSpace][colorSpace](
+            ...this.properties
+          );
+          this.properties = result;
+          this.properties[index] = value;
+          this.colorSpace = colorSpace;
+        };
+      });
+      if (this[colorSpace] === undefined) {
+        extendObservable(this, {
+          [colorSpace]: computed(() => {
+            const result = {};
+            colorSpaces[colorSpace].forEach(key => {
+              result[key] = this[key];
+            });
+            return result;
+          })
+        });
+      }
+    });
+  }
+  @computed
+  get colorName() {
+    return namer(this.hex, { pick: ["ntc"] }).ntc[0].name;
+  }
+  @computed
+  get contrastYIQ() {
+    return getContrastYIQ(`#${this.hex}`);
+  }
+}

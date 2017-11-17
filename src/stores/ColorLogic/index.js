94f20662c03bfaebf9e75183f99915500d5485a8
Thomas Trinca <tomtrinca@protonmail.com>
2017-11-17 11:33:17 +1100

    Update ColorLogic library index following removal of hexToHsl, hslToRgb, hslToHex, and rgbToHsl modules from library

--- src/stores/ColorLogic/index.js
+++ src/stores/ColorLogic/index.js
@@ -1,16 +1,12 @@
 export { default as analogous } from "./analogous";
 export { default as assembleColor } from "./assembleColor";
 export { default as getContrastYIQ } from "./getContrastYIQ";
-export { default as hexToHsl } from "./hexToHsl";
-export { default as hslToRgb } from "./hslToRgb";
-export { default as hslToHex } from "./hslToHex";
 export { default as oneOff } from "./oneOff";
 export { default as pentagon } from "./pentagon";
 export { default as random } from "./random";
 export { default as randomHue } from "./randomHue";
 export { default as randomLightness } from "./randomLightness";
 export { default as randomSaturation } from "./randomSaturation";
-export { default as rgbToHsl } from "./rgbToHsl";
 export { default as split } from "./split";
 export { default as square } from "./square";
 export { default as tetradic } from "./tetradic";

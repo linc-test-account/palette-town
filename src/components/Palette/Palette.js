65e50a2a22fcd8b71fcc6b3cadeac68fcdb982c9
Thomas Trinca <tomtrinca@protonmail.com>
2017-11-17 11:23:47 +1100

    Modify SortablieItem component to include "#" symbol infront of hex value

--- src/components/Palette/Palette.js
+++ src/components/Palette/Palette.js
@@ -4,6 +4,7 @@ import PropTypes from "prop-types";
 import { SortableContainer, SortableElement } from "react-sortable-hoc";
 import FontAwesome from "react-fontawesome";
 import TriangleDown from "../Elements/TriangleDown";
+import FlipMove from "react-flip-move";
 
 import "./Palette.css";
 
@@ -30,11 +31,10 @@ const SortableItem = SortableElement(
       color: `hsla(0, 0%, ${contrastYIQ}%, .4)`
     };
 
-
     return (
       <div className="palette-swatch" style={style}>
         <TriangleDown show={selected} />
-        <p className="palette-swatch-hex noselect">{hex}</p>
+        <p className="palette-swatch-hex noselect">#{hex}</p>
         <p className="palette-swatch-name noselect">{colorName}</p>
 
         <div className="palette-swatch-buttons-container">
@@ -64,10 +64,19 @@ const SortableItem = SortableElement(
 );
 
 const SortableList = SortableContainer(
-  observer(({ dataStore, items }) => {
+  observer(({ dataStore, items, sorting }) => {
     const sortableSwatches = items.map(
       (
-        { hue, saturation, lightness, hex, contrastYIQ, selected, colorName },
+        {
+          hue,
+          saturation,
+          lightness,
+          hex,
+          contrastYIQ,
+          selected,
+          colorName,
+          id
+        },
         index
       ) => (
         <SortableItem
@@ -79,33 +88,73 @@ const SortableList = SortableContainer(
           contrastYIQ={contrastYIQ}
           selected={selected}
           colorName={colorName}
-          key={`item-${index}`}
+          key={`item-${id}`}
           index={index}
           uniqueIndex={index}
         />
       )
     );
-    return <div className="palette-swatch-container">{sortableSwatches}</div>;
+    return (
+      <FlipMove
+        className="palette-swatch-container"
+        disableAllAnimations={sorting}
+        leaveAnimation={true}
+        easing="cubic-bezier(.4,-0.32,.52,1.31)"
+        duration={500}
+        maintainContainerHeight={true}
+        staggerDelayBy={40}
+      >
+        {sortableSwatches}
+      </FlipMove>
+    );
   })
 );
 
+// If state variable 'sorting' is true, user is moving individual
+// swatch via the SortableList component in palette class render
+// function. The 'sorting' state variable is passed onto FlipMove's
+// disableAllAnimations prop to disable animation of list order
+// change already handled by SortableList if it evaluates to
+// true
 @observer
 class palette extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      sorting: false
+    };
+  }
   static propTypes = {
     minWidthReached: PropTypes.bool,
     dataStore: PropTypes.object,
     currentPalette: PropTypes.object
   };
 
+  handleSortStart = () => {
+    this.setState({
+      sorting: true
+    });
+  };
+
+  handleSortEnd = object => {
+    this.props.dataStore.onSortEnd(object.oldIndex, object.newIndex);
+    this.setState({
+      sorting: false
+    });
+  };
+
   render() {
     const { minWidthReached, dataStore, currentPalette } = this.props;
+    const { sorting } = this.state;
     return (
       <SortableList
         axis={minWidthReached === true ? "y" : "x"}
         lockAxis={minWidthReached === true ? "y" : "x"}
         dataStore={dataStore}
         items={currentPalette}
-        onSortEnd={dataStore.onSortEnd}
+        sorting={sorting}
+        onSortStart={this.handleSortStart}
+        onSortEnd={this.handleSortEnd}
         pressDelay={150}
       />
     );

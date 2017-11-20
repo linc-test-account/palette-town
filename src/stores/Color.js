e99c5ef452d85476fc568acc22c001f06dfb025d
Thomas Trinca <tomtrinca@protonmail.com>
2017-11-20 12:52:57 +1100

    Add selected observable to Color class of type boolean

--- src/stores/Color.js
+++ src/stores/Color.js
@@ -14,6 +14,7 @@ const colorSpaces = {
 
 export default class Color {
   @observable properties = [];
+  @observable selected = false;
   @observable colorSpace = "rgb";
   @observable id = shortid.generate();
 

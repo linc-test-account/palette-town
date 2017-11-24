82682f974081a0b494f56e9a0d1e0aa0d6927622
Thomas Trinca <tomtrinca@protonmail.com>
2017-11-24 23:33:48 +1100

    Add colorHarmonies module to ColorLogic directory

---
+++ src/stores/ColorLogic/colorHarmonies.js
@@ -0,0 +1,11 @@
+const colorHarmonies = [
+    { harmony: "analogous", colors: 3 },
+    { harmony: "pentagon", colors: 5 },
+    { harmony: "random", colors: 5 },
+    { harmony: "split", colors: 3 },
+    { harmony: "square", colors: 4 },
+    { harmony: "tetradic", colors: 4 },
+    { harmony: "triadic", colors: 3 }
+  ];
+
+  module.exports = colorHarmonies;

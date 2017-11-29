699e558227a5c756ba4b4d3cdc57fc463e202be2
Thomas Trinca <tomtrinca@protonmail.com>
2017-11-30 00:17:18 +1100

    Add copyright decleration to footer component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { observer } from "mobx-react";
+// import FontAwesome from "react-fontawesome";
 import "./Footer.css";
 
 @observer
@@ -13,12 +14,18 @@ class Footer extends Component {
     const { dataStore } = this.props;
     return (
       <div className="footer">
+      <div className="footer-left">
         <h2 className="footer-subheading">Harmony:</h2>
         <h2 className="footer-subheading-value">{dataStore.selectedHarmony.harmony}</h2>
         <h2 className="footer-subheading">Modifier:</h2>
         <h2 className="footer-subheading-value">{dataStore.selectedPaletteModifier.modifier}</h2>
         <h2 className="footer-subheading">Favorites:</h2>
         <h2 className="footer-subheading-value">{dataStore.favorites.length}</h2>
+      </div>
+        <div className="footer-right">
+          Copyright &copy; 2017 <a className="footer-link" href="https://github.com/plexey">Thomas Trinca</a>
+          {/* <FontAwesome className="footer-code-icon" name="code" size="2x" /> */}
+        </div>
       </div>
     );
   }

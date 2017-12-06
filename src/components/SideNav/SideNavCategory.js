ee39600547cbf81465273e71fb53efa63edd1aed
Thomas Trinca <tomtrinca@protonmail.com>
2017-12-07 00:15:43 +1100

    Add SideNavCategory component

---
+++ src/components/SideNav/SideNavCategory.js
@@ -0,0 +1,58 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { observer } from "mobx-react";
+import FontAwesome from "react-fontawesome";
+import "./SideNav.css";
+
+@observer
+class SideNavCategory extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isExpanded: true
+    };
+  }
+
+  toggleIsExpanded = () => {
+    const { isExpanded } = this.state;
+    this.setState({
+      isExpanded: isExpanded === true ? false : true
+    });
+  };
+
+  static propTypes = {
+    categoryName: PropTypes.string,
+    categoryItems: PropTypes.array
+  };
+
+  render() {
+    const { isExpanded } = this.state;
+    const { categoryName, categoryItems } = this.props;
+    const submenuStyleDefault = {
+      maxHeight: "0"
+    };
+    const submenuOpen = {
+      maxHeight: "1000px"
+    };
+    return (
+      <div>
+        <h2 className="sidenav-category" onClick={this.toggleIsExpanded}>
+          {categoryName}
+          <FontAwesome
+            className="sidenav-category-caret"
+            name={isExpanded === true ? "caret-up" : "caret-down"}
+            size="2x"
+          />
+        </h2>
+        <div
+          className="sidenav-category-container"
+          style={isExpanded === true ? submenuOpen : submenuStyleDefault}
+        >
+          {categoryItems.length > 0 ? categoryItems : ""}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default SideNavCategory;

c46bdaa5a47eef9abb5ba4a50ff9f90fbf73e8cd
Thomas Trinca <tomtrinca@protonmail.com>
2017-12-07 00:23:53 +1100

    Add HeaderButton tests

---
+++ src/components/HeaderButton/HeaderButton.test.js
@@ -0,0 +1,35 @@
+import HeaderButton from "./HeaderButton";
+
+import React from "react";
+import { shallow } from "enzyme";
+
+describe("HeaderButton", () => {
+  let props;
+  beforeEach(() => {
+    props = {
+      btnFunction: jest.fn(),
+      fontAwesomeIcon: "exampleIcon",
+      overlayValue: 1,
+      isActive: true
+    };
+  });
+  
+  it("should render active", () => {
+    const wrapper = shallow(<HeaderButton {...props} isActive={true} />);
+    expect(wrapper).toMatchSnapshot();
+  });
+  
+  it("should render in-active", () => {
+    const wrapper = shallow(<HeaderButton {...props} isActive={false} />);
+    expect(wrapper).toMatchSnapshot();
+  });
+  
+  it("should call when clicked", () => {
+
+    const wrapper = shallow(<HeaderButton {...props} isActive={false} />);
+    
+    wrapper.find('.header-button').simulate('click');
+
+    expect(props.btnFunction).toHaveBeenCalledTimes(1);
+  })
+});

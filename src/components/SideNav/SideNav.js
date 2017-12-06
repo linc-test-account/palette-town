e76c879d18084e10854364761e50ffafa4b4c964
Thomas Trinca <tomtrinca@protonmail.com>
2017-12-07 00:15:18 +1100

    Refactor SideNav component to abstract side nav categories out into seperate component

--- src/components/SideNav/SideNav.js
+++ src/components/SideNav/SideNav.js
@@ -3,106 +3,82 @@ import PropTypes from "prop-types";
 import { observer } from "mobx-react";
 import MiniPalette from "../MiniPalette/MiniPalette";
 import FontAwesome from "react-fontawesome";
+import Modal from "react-modal";
 import HeaderButton from "../HeaderButton/HeaderButton";
 import OutsideAlerter from "../ClickOutside/ClickOutside";
+import PaletteDetails from "../ModalContent/PaletteDetails";
+import SideNavCategory from "./SideNavCategory";
 import "./SideNav.css";
 
 function getHarmonies(harmonies, dataStore) {
-  const harmonyList = harmonies.map(
-    ({ harmony }, index) =>
-      harmony === dataStore.selectedHarmony.harmony ? (
-        <a
-          className="drop-down-list-option list-option-selected"
-          key={`harmony-${index}`}
-        >
-          {harmony}
-          <MiniPalette
-            swatchWidth={20}
-            swatchHeight={15}
-            harmony={dataStore.miniPalettes[index][harmony]}
-          />
-        </a>
-      ) : (
-        <a
-          className="drop-down-list-option"
-          key={`harmony-${index}`}
-          onClick={() => {
-            dataStore.changeHarmony(index);
-          }}
-        >
-          {harmony}
-          <MiniPalette
-            swatchWidth={20}
-            swatchHeight={15}
-            harmony={dataStore.miniPalettes[index][harmony]}
-          />
-        </a>
-      )
-  );
+  const harmonyList = harmonies.map(({ harmony }, index) => (
+    <a
+      className={`drop-down-list-option ${
+        harmony === dataStore.selectedHarmony.harmony
+          ? "list-option-selected"
+          : ""
+      }`}
+      key={`harmony-${index}`}
+      onClick={() => {
+        dataStore.changeHarmony(index);
+      }}
+    >
+      {harmony}
+      <MiniPalette
+        swatchWidth={20}
+        swatchHeight={15}
+        harmony={dataStore.miniPalettes[index][harmony]}
+      />
+    </a>
+  ));
   return harmonyList;
 }
 
 function getmodifiers(modifiers, dataStore) {
-  const harmonyList = modifiers.map(
-    ({ modifier }, index) =>
-      modifier === dataStore.selectedModifier.modifier ? (
-        <a
-          className="drop-down-list-option list-option-selected"
-          key={`modifier-${index}`}
-        >
-          {modifier}
-        </a>
-      ) : (
-        <a
-          className="drop-down-list-option"
-          key={`modifier-${index}`}
-          onClick={() => {
-            dataStore.changeModifier(index);
-          }}
-        >
-          {modifier}
-        </a>
-      )
-  );
+  const harmonyList = modifiers.map(({ modifier }, index) => (
+    <a
+      className={`drop-down-list-option ${
+        modifier === dataStore.selectedModifier.modifier
+          ? "list-option-selected"
+          : ""
+      }`}
+      key={`modifier-${index}`}
+      onClick={() => {
+        dataStore.changeModifier(index);
+      }}
+    >
+      {modifier}
+    </a>
+  ));
   return harmonyList;
 }
 
-function getFavorites(favorites, dataStore) {
-  const items = [];
-  for (let i = 0; i < favorites.length; i++) {
-    items.push(
-      <div key={`favorite-item-${i}`} className="favorite-list-item-container">
-        <a className="drop-down-list-option">
-          <MiniPalette
-            swatchWidth={20}
-            swatchHeight={15}
-            harmony={favorites[i]}
-          />
-        </a>
-        <button
-          onClick={() => dataStore.deleteFromFavorites(i)}
-          className="favorite-list-item-button"
-        >
-          <FontAwesome
-            className="favorite-list-item-icon"
-            name={"trash"}
-            size="2x"
-          />
-        </button>
-        <button
-          onClick={() => dataStore.goToPalette(i)}
-          className="favorite-list-item-button"
-        >
-          <FontAwesome
-            className="favorite-list-item-icon"
-            name={"pencil"}
-            size="2x"
-          />
-        </button>
+function getFavorites(favorites, favoritesShortList, dataStore, handleClick) {
+  return favorites.map(({ name }, index) => (
+    <div key={`favorite-item-${index}`} className="favorite-item-container">
+      <div
+        className="favorite-item"
+        onClick={() => dataStore.goToPalette(index)}
+      >
+        <p className="favorite-name">{name}</p>
+        <MiniPalette
+          swatchWidth={20}
+          swatchHeight={15}
+          harmony={favoritesShortList[index]}
+        />
       </div>
-    );
-  }
-  return items;
+      <button
+        onClick={() => handleClick(index)}
+        className="favorite-list-item-button"
+      >
+        <FontAwesome
+          className="favorite-list-item-icon"
+          name={"cog"}
+          size="2x"
+        />
+      </button>
+    </div>
+  ));
 }
 
 @observer
@@ -110,70 +86,40 @@ class SideNav extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      showHarmonies: true,
-      showModifiers: true,
-      showFavorites: true
+      isShowingModal: false,
+      targetFavorite: undefined
     };
   }
 
   static propTypes = {
     dataStore: PropTypes.object,
     showSideNav: PropTypes.bool,
-    toggleSideNav: PropTypes.func
+    toggleSideNav: PropTypes.func,
+    toggleSideNavVisibility: PropTypes.func
   };
 
-  toggleHarmonies = () => {
-    const { showHarmonies } = this.state;
-    if (showHarmonies === true) {
-      this.setState({
-        showHarmonies: false
-      });
-    } else {
-      this.setState({
-        showHarmonies: true
-      });
-    }
+  handleClick = val => {
+    const { toggleSideNavVisibility } = this.props;
+    toggleSideNavVisibility();
+    this.setState({
+      isShowingModal: true,
+      targetFavorite: val
+    });
   };
 
-  toggleModifiers = () => {
-    const { showModifiers } = this.state;
-    if (showModifiers === true) {
-      this.setState({
-        showModifiers: false
-      });
-    } else {
-      this.setState({
-        showModifiers: true
-      });
-    }
-  };
-
-  toggleFavorites = () => {
-    const { showFavorites } = this.state;
-    if (showFavorites === true) {
-      this.setState({
-        showFavorites: false
-      });
-    } else {
-      this.setState({
-        showFavorites: true
-      });
-    }
+  handleClose = () => {
+    const { toggleSideNavVisibility } = this.props;
+    this.setState({ isShowingModal: false });
+    toggleSideNavVisibility();
   };
 
   render() {
     const { dataStore, toggleSideNav, showSideNav } = this.props;
-    const { showHarmonies, showModifiers, showFavorites } = this.state;
+    const { isShowingModal } = this.state;
     const style = {
       transform: showSideNav === false ? "translateX(-300px)" : "translateX(0)"
     };
 
-    const submenuStyleDefault = {
-      maxHeight: "0"
-    };
-    const submenuOpen = {
-      maxHeight: "1000px"
-    };
     return (
       <div style={style} id="mySidenav" className="sidenav">
         <OutsideAlerter showSideNav={showSideNav} toggleSideNav={toggleSideNav}>
@@ -189,65 +135,47 @@ class SideNav extends Component {
               <h1 className="mobile-brand-name">PT</h1>
             </div>
 
-            <h2
-              key={`sidenave-heading-${0}`}
-              className="sidenav-category"
-              onClick={this.toggleHarmonies}
-            >
-              Harmonies
-              <FontAwesome
-                className="sidenav-category-caret"
-                name={showHarmonies === true ? "caret-up" : "caret-down"}
-                size="2x"
-              />
-            </h2>
-            <div
-              className="sidenav-category-container"
-              style={showHarmonies === true ? submenuOpen : submenuStyleDefault}
-            >
-              {getHarmonies(dataStore.harmonies, dataStore)}
-            </div>
+            <SideNavCategory
+              categoryName="Harmonies"
+              categoryItems={getHarmonies(dataStore.harmonies, dataStore)}
+            />
 
-            <h2
-              key={`sidenave-heading-${1}`}
-              className="sidenav-category"
-              onClick={this.toggleModifiers}
-            >
-              Modifiers
-              <FontAwesome
-                className="sidenav-category-caret"
-                name={showModifiers === true ? "caret-up" : "caret-down"}
-                size="2x"
-              />
-            </h2>
-            <div
-              className="sidenav-category-container"
-              style={showModifiers === true ? submenuOpen : submenuStyleDefault}
-            >
-              {getmodifiers(dataStore.modifiers, dataStore)}
-            </div>
-            <h2
-              key={`sidenave-heading-${2}`}
-              className="sidenav-category"
-              onClick={this.toggleFavorites}
-            >
-              Favorites
-              <FontAwesome
-                className="sidenav-category-caret"
-                name={showFavorites === true ? "caret-up" : "caret-down"}
-                size="2x"
-              />
-            </h2>
-            <div
-              className="sidenav-category-container"
-              style={showFavorites === true ? submenuOpen : submenuStyleDefault}
-            >
-              {dataStore.favorites.length > 0 ? (
-                getFavorites(dataStore.favoritesShortList, dataStore)
-              ) : (
-                <a className="drop-down-list-option">No Favorites</a>
+            <SideNavCategory
+              categoryName="Modifiers"
+              categoryItems={getmodifiers(dataStore.modifiers, dataStore)}
+            />
+
+            <SideNavCategory
+              categoryName="Favorites"
+              categoryItems={getFavorites(
+                dataStore.favorites,
+                dataStore.favoritesShortList,
+                dataStore,
+                this.handleClick
               )}
-            </div>
+            />
+            <div className="sidenav-spacer" />
+            <Modal
+              isOpen={isShowingModal}
+              onRequestClose={this.handleClose}
+              contentLabel="Color Info Modal"
+              className={{
+                base: "colorModalDialog",
+                afterOpen: "colorModalDialog_after-open",
+                beforeClose: "colorModalDialog_before-close"
+              }}
+              overlayClassName={{
+                base: "modalOverlay",
+                afterOpen: "modalOverlay_after-open",
+                beforeClose: "modalOverlay_before-close"
+              }}
+            >
+              <PaletteDetails
+                targetFavorite={this.state.targetFavorite}
+                dataStore={dataStore}
+                handleClose={this.handleClose}
+              />
+            </Modal>
           </div>
         </OutsideAlerter>
       </div>

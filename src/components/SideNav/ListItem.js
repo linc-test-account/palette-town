ca797db5501f1984d97398ede3bb16e7b708483e
Thomas Trinca <tomtrinca@protonmail.com>
2017-12-12 23:30:01 +1100

    Add ListItem component

---
+++ src/components/SideNav/ListItem.js
@@ -0,0 +1,50 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { observer } from "mobx-react";
+import styles from "./ListItem.css";
+import classNames from "classnames";
+import FontAwesome from "react-fontawesome";
+
+@observer
+class ListItem extends Component {
+  static propTypes = {
+    isActive: PropTypes.bool,
+    action: PropTypes.func,
+    children: PropTypes.any,
+    showControls: PropTypes.bool,
+    controlAction: PropTypes.func
+  };
+
+  render() {
+    const {
+      isActive,
+      action,
+      children,
+      showControls,
+      controlAction
+    } = this.props;
+    return (
+      <div
+        className={classNames({
+          [styles.container]: true,
+          [styles.active]: isActive
+        })}
+      >
+        <div onClick={action} className={styles.content}>
+          {children}
+        </div>
+        <div
+          onClick={controlAction}
+          className={classNames({
+            [styles.controls]: true,
+            [styles.controlsVisible]: showControls
+          })}
+        >
+          <FontAwesome className={styles.icon} name={"cog"} size="2x" />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ListItem;

8a646993841bda6b15f343311dd1c84485f98643
Thomas Trinca <tomtrinca@protonmail.com>
2017-12-15 23:28:30 +1100

    Rename all instances of "modifier" to "name"

--- src/stores/ColorLogic/modifiers.js
+++ src/stores/ColorLogic/modifiers.js
@@ -1,48 +1,48 @@
 const modifiers = [
     {
-      modifier: "none",
+      name: "none",
       saturationMin: 0,
       saturationMax: 100,
       lightnessMin: 0,
       lightnessMax: 100
     },
     {
-      modifier: "balanced",
+      name: "balanced",
       saturationMin: 30,
       saturationMax: 80,
       lightnessMin: 50,
       lightnessMax: 90
     },
     {
-      modifier: "pastel",
+      name: "pastel",
       saturationMin: 50,
       saturationMax: 100,
       lightnessMin: 60,
       lightnessMax: 90
     },
     {
-      modifier: "somber",
+      name: "somber",
       saturationMin: 0,
       saturationMax: 40,
       lightnessMin: 20,
       lightnessMax: 40
     },
     {
-      modifier: "saturated",
+      name: "saturated",
       saturationMin: 100,
       saturationMax: 100,
       lightnessMin: 50,
       lightnessMax: 50
     },
     {
-      modifier: "desaturated",
+      name: "desaturated",
       saturationMin: 30,
       saturationMax: 0,
       lightnessMin: 0,
       lightnessMax: 100
     },
     {
-      modifier: "greyscale",
+      name: "greyscale",
       saturationMin: 0,
       saturationMax: 0,
       lightnessMin: 20,

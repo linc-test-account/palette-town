d7e7960821c51b08ebcf156febf8623c98bcbece
Thomas Trinca <tomtrinca@protonmail.com>
2017-12-15 23:28:01 +1100

    Replace random function call with uniqueRandom

--- src/stores/ColorLogic/getPalette.js
+++ src/stores/ColorLogic/getPalette.js
@@ -1,39 +1,38 @@
 const analogous = require("./analogous");
-const random = require("./random");
 const split = require("./split");
 const square = require("./square");
 const tetradic = require("./tetradic");
 const triadic = require("./triadic");
 const pentagon = require("./pentagon");
+const uniqueRandom = require("./UniqueRandom");
 
 function getPalette(harmony, modifier) {
-    let info;
-    switch (harmony) {
-      case "analogous":
-        info = analogous(modifier);
-        break;
-      case "random":
-        info = random(modifier);
-        break;
-      case "split":
-        info = split(modifier);
-        break;
-      case "square":
-        info = square(modifier);
-        break;
-      case "tetradic":
-        info = tetradic(modifier);
-        break;
-      case "triadic":
-        info = triadic(modifier);
-        break;
-      case "pentagon":
-        info = pentagon(modifier);
-        break;
-      default:
-    }
-    return info;
+  let info;
+  switch (harmony) {
+    case "analogous":
+      info = analogous(modifier);
+      break;
+    case "random":
+      info = uniqueRandom(modifier);
+      break;
+    case "split":
+      info = split(modifier);
+      break;
+    case "square":
+      info = square(modifier);
+      break;
+    case "tetradic":
+      info = tetradic(modifier);
+      break;
+    case "triadic":
+      info = triadic(modifier);
+      break;
+    case "pentagon":
+      info = pentagon(modifier);
+      break;
+    default:
   }
-  
-  module.exports = getPalette;
-  
+  return info;
+}
+
+module.exports = getPalette;

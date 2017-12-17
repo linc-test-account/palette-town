1cbf1d1ba64255fd7e535c121e015fc82c22c2f1
Thomas Trinca <tomtrinca@protonmail.com>
2017-12-18 00:57:14 +1100

    Remove deprecated modules

--- src/stores/ColorLogic/index.js
+++ src/stores/ColorLogic/index.js
@@ -1,16 +1,7 @@
-export { default as analogous } from "./analogous";
 export { default as assembleColor } from "./assembleColor";
 export { default as harmonies } from "./harmonies";
+export { default as generateHarmony } from "./generateHarmony";
 export { default as getContrastYIQ } from "./getContrastYIQ";
 export { default as getPalette } from "./getPalette";
 export { default as oneOff } from "./oneOff";
 export { default as modifiers } from "./modifiers";
-export { default as pentagon } from "./pentagon";
-export { default as random } from "./random";
-export { default as randomHue } from "./randomHue";
-export { default as randomLightness } from "./randomLightness";
-export { default as randomSaturation } from "./randomSaturation";
-export { default as split } from "./split";
-export { default as square } from "./square";
-export { default as tetradic } from "./tetradic";
-export { default as triadic } from "./triadic";

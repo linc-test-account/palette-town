ef9967a85ec5a1e13ddb4015cfcd11ae5a796cd9
Thomas Trinca <tomtrinca@protonmail.com>
2017-12-18 00:56:37 +1100

    Refactor module data structure

--- src/stores/ColorLogic/harmonies.js
+++ src/stores/ColorLogic/harmonies.js
@@ -1,11 +1,11 @@
-const harmonies = [
-    { harmony: "analogous", colors: 3 },
-    { harmony: "pentagon", colors: 5 },
-    { harmony: "random", colors: 5 },
-    { harmony: "split", colors: 3 },
-    { harmony: "square", colors: 4 },
-    { harmony: "tetradic", colors: 4 },
-    { harmony: "triadic", colors: 3 }
-  ];
+const harmonies = {
+  analagous: { name: "analagous", coordinates: [0, 60, 120] },
+  pentagon: { name: "pentagon", coordinates: [0, 72, 144, 216, 288] },
+  random: { name: "random", coordinates: [] },
+  split: { name: "split", coordinates: [0, 150, 300] },
+  square: { name: "square", coordinates: [90, 180, 270, 360] },
+  tetradic: { name: "tetradic", coordinates: [0, 50, 180, 230] },
+  triadic: { name: "triadic", coordinates: [0, 120, 240] }
+};
 
-  module.exports = harmonies;
+module.exports = harmonies;
--- src/stores/ColorLogic/modifiers.js
+++ src/stores/ColorLogic/modifiers.js
@@ -1,53 +1,53 @@
-const modifiers = [
-    {
-      name: "none",
-      saturationMin: 0,
-      saturationMax: 100,
-      lightnessMin: 0,
-      lightnessMax: 100
-    },
-    {
-      name: "balanced",
-      saturationMin: 30,
-      saturationMax: 80,
-      lightnessMin: 50,
-      lightnessMax: 90
-    },
-    {
-      name: "pastel",
-      saturationMin: 50,
-      saturationMax: 100,
-      lightnessMin: 60,
-      lightnessMax: 90
-    },
-    {
-      name: "somber",
-      saturationMin: 0,
-      saturationMax: 40,
-      lightnessMin: 20,
-      lightnessMax: 40
-    },
-    {
-      name: "saturated",
-      saturationMin: 100,
-      saturationMax: 100,
-      lightnessMin: 50,
-      lightnessMax: 50
-    },
-    {
-      name: "desaturated",
-      saturationMin: 30,
-      saturationMax: 0,
-      lightnessMin: 0,
-      lightnessMax: 100
-    },
-    {
-      name: "greyscale",
-      saturationMin: 0,
-      saturationMax: 0,
-      lightnessMin: 20,
-      lightnessMax: 90
-    }
-  ];
+const modifiers = {
+  none: {
+    name: "none",
+    saturationMin: 0,
+    saturationMax: 100,
+    lightnessMin: 0,
+    lightnessMax: 100
+  },
+  balanced: {
+    name: "balanced",
+    saturationMin: 30,
+    saturationMax: 80,
+    lightnessMin: 50,
+    lightnessMax: 90
+  },
+  pastel: {
+    name: "pastel",
+    saturationMin: 50,
+    saturationMax: 100,
+    lightnessMin: 60,
+    lightnessMax: 90
+  },
+  somber: {
+    name: "somber",
+    saturationMin: 0,
+    saturationMax: 40,
+    lightnessMin: 20,
+    lightnessMax: 40
+  },
+  saturated: {
+    name: "saturated",
+    saturationMin: 100,
+    saturationMax: 100,
+    lightnessMin: 50,
+    lightnessMax: 50
+  },
+  desaturated: {
+    name: "desaturated",
+    saturationMin: 30,
+    saturationMax: 0,
+    lightnessMin: 0,
+    lightnessMax: 100
+  },
+  greyscale: {
+    name: "greyscale",
+    saturationMin: 0,
+    saturationMax: 0,
+    lightnessMin: 20,
+    lightnessMax: 90
+  }
+};
 
-  module.exports = modifiers;
+module.exports = modifiers;

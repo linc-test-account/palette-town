ea29b99fda899e9b6cb95cc916f5e3ad3817c54f
Thomas Trinca <tomtrinca@protonmail.com>
2017-12-18 00:58:02 +1100

    Refactor method Remove deprecated module imports

--- src/stores/ColorLogic/assembleColor.js
+++ src/stores/ColorLogic/assembleColor.js
@@ -1,22 +1,19 @@
-const randomSaturation = require("./randomSaturation");
-const randomLightness = require("./randomLightness");
-
-function assembleColor(colors, modifier) {
+function assembleColor(hues, modifier) {
   if (modifier) {
     const SAT_MAX = modifier.saturationMax;
     const SAT_MIN = modifier.saturationMin;
     const LIT_MAX = modifier.lightnessMax;
     const LIT_MIN = modifier.lightnessMin;
-    return colors.map(item => ({
-      hue: item,
+    return hues.map(hue => ({
+      hue: hue,
       saturation: Math.floor(Math.random() * (SAT_MAX - SAT_MIN) + SAT_MIN),
       lightness: Math.floor(Math.random() * (LIT_MAX - LIT_MIN) + LIT_MIN)
     }));
   } else {
-    return colors.map(item => ({
-      hue: item,
-      saturation: randomSaturation(),
-      lightness: randomLightness()
+    return hues.map(hue => ({
+      hue: hue,
+      saturation: Math.floor(Math.random() * (0 - 100) + 100),
+      lightness: Math.floor(Math.random() * (0 - 100) + 100)
     }));
   }
 }

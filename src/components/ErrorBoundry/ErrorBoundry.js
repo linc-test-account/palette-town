bc315ba80cfa38432b24d8cbf22cf3f06562c08d
Thomas Trinca <tomtrinca@protonmail.com>
2017-12-18 00:52:04 +1100

    Add ErrorBoundry component

---
+++ src/components/ErrorBoundry/ErrorBoundry.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+class ErrorBoundary extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static propTypes = {
+    children: PropTypes.element.isRequired
+  };
+
+  componentDidCatch() {
+    // Display fallback UI
+    this.setState({ hasError: true });
+    // You can also log the error to an error reporting service
+    // logErrorToMyService(error, info);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      // You can render any custom fallback UI
+      return <h1>Something went wrong.</h1>;
+    }
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;

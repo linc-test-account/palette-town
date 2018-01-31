b601df66debb041341f96b77158fa25f07a8857b
Thomas Trinca <tomtrinca@protonmail.com>
2018-01-31 19:58:17 +1100

    Add documentation Replace magic numbers with named variables

--- src/stores/ColorLogic/generateHarmony.js
+++ src/stores/ColorLogic/generateHarmony.js
@@ -1,10 +1,21 @@
+/**
+ * generateHarmony
+ *
+ * Takes array of numbers corresponding to coordinates on
+ * a color wheel and maps them to a new array keyed off
+ * an initial randomly generated value named initialValue
+ *
+ * @param {array}   harmony           Array of numbers.
+ *
+ * @returns {array} New array.
+ */
+
 function generateHarmony(harmony) {
   const HUES = 360;
   const initialValue = Math.floor(Math.random() * HUES);
-
   return harmony.map(value => {
-    return initialValue + value > 360
-      ? initialValue + value - 360
+    return initialValue + value > HUES
+      ? initialValue + value - HUES
       : initialValue + value;
   });
 }

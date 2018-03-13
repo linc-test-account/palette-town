4d01bfd14f5f325c3c4ca3db8abf8cb092326f53
plexey <tptrinca@gmail.com>
2018-03-13 15:49:36 +1100

    refactor: convert to functional component feat: add support for button Icon

--- src/components/AppButton/AppButton.js
+++ src/components/AppButton/AppButton.js
@@ -1,30 +1,30 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import styles from "./AppButton.css";
 import classNames from "classnames";
 
-class AppButton extends Component {
-  static propTypes = {
-    buttonText: PropTypes.string.isRequired,
-    buttonType: PropTypes.string.isRequired,
-    buttonAction: PropTypes.func
-  };
+const AppButton = ({ buttonText, buttonIcon, buttonType, buttonAction }) => (
+  <button
+    onClick={buttonAction}
+    className={classNames({
+      [styles.button]: true,
+      [styles.confirm]: buttonType === "confirm",
+      [styles.danger]: buttonType === "danger"
+    })}
+  >
+    <i className={classNames({
+      ["material-icons"]: true,
+      [styles.icon]: true
+    })}>{buttonIcon}</i>
+    {buttonText}
+  </button>
+);
 
-  render() {
-    const { buttonText, buttonType, buttonAction } = this.props;
-    return (
-      <button
-        onClick={buttonAction}
-        className={classNames({
-          [styles.button]: true,
-          [styles.confirm]: buttonType === "confirm",
-          [styles.danger]: buttonType === "danger"
-        })}
-      >
-        {buttonText}
-      </button>
-    );
-  }
-}
+AppButton.propTypes = {
+  buttonText: PropTypes.string.isRequired,
+  buttonIcon: PropTypes.string,
+  buttonType: PropTypes.string.isRequired,
+  buttonAction: PropTypes.func
+};
 
 export default AppButton;
